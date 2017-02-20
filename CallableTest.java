347f3332ae576c3553fc9f6a95520003b5d3ec3f
vitaliysharg <vitaliysharg@ukr.net>
2017-02-20 18:27:28 +0200

    test commit

---
+++ CallableTest.java
@@ -0,0 +1,21 @@
+package com.vit;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+public class CallableTest {
+
+	public static void main(String[] args) throws InterruptedException, ExecutionException {
+		ExecutorService service = null;
+		try {
+			service = Executors.newSingleThreadExecutor();
+			Future<Integer> result = service.submit(() -> 30 + 11);
+			System.out.println(result.get());
+		} finally {
+			if (service != null)
+				service.shutdown();
+		}
+	}
+}
